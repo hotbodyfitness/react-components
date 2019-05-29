c9c0fcdcaf3952838b7511e6e3528d890f94be39
Zack Miller <hotbodyfitness@gmail.com>
2019-05-29 12:43:53 -0700

    React Components complete

--- app.js
+++ app.js
@@ -1,42 +1,89 @@
+// // TODO
+// //we get rid of the parens and implicit return
+// //and add in an explicit return statement because we have more than 1 item
+// class GroceryListItem extends React.Component {
+
+//   constructor(props) {
+//     console.log('line 6', props);
+//     super(props);
+
+//   }
+//   render() {
+//     return (
+//       <li>{this.props.asdf}</li>
+//     );
+//   }
+
+// }
+
+// var GroceryList = (props) => {
+
+//   var items = ['apple', 'orange'];
+
+//   render() {
+//     return (
+//   <ul>
+//     {console.log(items)}
+//     {items.map(groceryItem => {
+//       console.log(props)
+
+//       return (<GroceryListItem asdf ={groceryItem}/> )
+//     }
+//     )}
+//   </ul>
+
+//   )
+//   }
+
+// }
+
+// ReactDOM.render(<GroceryList /*items = {['apple', 'orange']} */ />, document.getElementById('app'));
+
+
+
 // TODO
-//we get rid of the parens and implicit return
-//and add in an explicit return statement because we have more than 1 item
+// we get rid of the parens and implicit return
+// and add in an explicit return statement because we have more than 1 item
 class GroceryListItem extends React.Component {
   constructor(props) {
+    console.log('line 49', props);
     super(props);
+    this.state = {
+      done: false
+    };
+
   }
+
+  onListItemHover(){
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
   render() {
+
+    var style ={
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+
     return (
-      <li>{this.props.groceryItem}</li>
+      <li style = {style} onMouseOver = {this.onListItemHover.bind(this)}>{this.props.asdf}</li>
     );
   }
 
-  // <ul>
-  //   <li>{this.props.groceryItems[0]}</li>
-  //   <li>{props.groceryItems[1]}</li>
-  // </ul>
-
 }
-//where are defining items?
+
 var GroceryList = (props) => (
-  // items = ['apple', 'orange'];
+
   <ul>
-    {props.items.map(groceryItem =>
-      <GroceryListItem groceryItem={groceryItem} />
+    {console.log('line 65', props)}
+    {props.items.map(groceryItem => {
+      return(<GroceryListItem asdf ={groceryItem} />)
+    }
     )}
   </ul>
 );
 
-
-// var App = () => (
-//   <div>
-//     <h2>My Todo List</h2>
-//     <GroceryList items={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
-//   </div>
-// );
-
-ReactDOM.render(<GroceryListItem />, document.getElementById('app'));
-
-
-
+ReactDOM.render(<GroceryList items = {['chicken', 'beef']}/>, document.getElementById('app'));
 
